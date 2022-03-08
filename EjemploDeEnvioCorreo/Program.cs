a4016b2e3cc927aa32efb28c2011bd335782ce2e
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-07 22:14:08 -0600

    Se agrega método Send

--- EjemploDeEnvioCorreo/Program.cs
+++ EjemploDeEnvioCorreo/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace EjemploDeEnvioCorreo
 {
@@ -7,6 +12,35 @@ namespace EjemploDeEnvioCorreo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var res = Send();
+            Console.WriteLine(res);
+        }
+
+        public static bool Send()
+        {
+            try
+            {
+                
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("");
+                mail.From = new MailAddress("");
+                mail.To.Add("");
+                mail.Subject = "";
+                mail.Body = "<p><p>";
+                mail.IsBodyHtml = true;
+                SmtpServer.Port = 465;
+       
+                SmtpServer.Credentials = new System.Net.NetworkCredential("", "");
+                SmtpServer.EnableSsl = false;
+                SmtpServer.Send(mail);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
